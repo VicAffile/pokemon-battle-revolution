f3f03c43a9848f1c0f5be2473f8dfa039c12214b
VicAffile <victoraffile@gmail.com>
2021-09-13 11:38:30 +0200

    création api, ajout sprite et affichage sur l'index

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const express = require('express');
 const swig = require('swig');
 const path = require('path');
 const bodyParser = require('body-parser');
+const pokemons = require('./json/pokemons.json');
+
 
 let app = express();
 app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+
 app.engine('html', swig.renderFile);
+
+
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+
 app.get('/', function(req, res) {
-    let data = { titre: 'Liste des Pokémons' };
-    res.render('index', data);
+    console.log(pokemons);
+    let donnee = { titre: 'Liste des Pokémons', pokemons: pokemons };
+    res.render('index', donnee);
 });
+
 
 app.listen(8080);
 console.log("L'application tourne.");
