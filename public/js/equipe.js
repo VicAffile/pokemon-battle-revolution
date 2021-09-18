e4201d267423b4a6190ce9a552ab3cba51e2f2ce
VicAffile <victoraffile@gmail.com>
2021-09-18 20:35:25 +0200

    fonction surprimer complete

--- public/js/equipe.js
+++ public/js/equipe.js
@@ -1,4 +1,3 @@
-//localStorage.removeItem('mon_equipe_pokemon_battle_revolution')
 if (localStorage.getItem('mon_equipe_pokemon_battle_revolution') == undefined) {
     localStorage.setItem('mon_equipe_pokemon_battle_revolution', JSON.stringify({
         pokemon_1: [2, "/images/sprites/dracaufeu.png"],
@@ -29,6 +28,31 @@ if (localStorage.getItem('mon_equipe_pokemon_battle_revolution') == undefined) {
 
 function supprimer(bouton) {
     let pokemon = bouton.parentNode.parentNode;
+    let equipe = JSON.parse(localStorage.getItem("mon_equipe_pokemon_battle_revolution"));
+    switch (pokemon.id) {
+        case 'un':
+            equipe.pokemon_1 = [0, ""];
+            break;
+        case 'deux':
+            equipe.pokemon_2 = [0, ""];
+            break;
+        case 'trois':
+            equipe.pokemon_3 = [0, ""];
+            break;
+        case 'quatre':
+            equipe.pokemon_4 = [0, ""];
+            break;
+        case 'cinq':
+            equipe.pokemon_5 = [0, ""];
+            break;
+        case 'six':
+            equipe.pokemon_6 = [0, ""];
+            break;
+        default:
+            console.log("L'id n'est pas présent");
+    }
+    localStorage.removeItem('mon_equipe_pokemon_battle_revolution');
+    localStorage.setItem('mon_equipe_pokemon_battle_revolution', JSON.stringify(equipe));
     pokemon.childNodes[1].classList.replace('sprite', 'sprite-pokeball');
     pokemon.childNodes[1].innerHTML = '<img src="/images/ressources/pokeball.png" alt="Sprite du Pokémon">';
     pokemon.childNodes[3].classList.replace('boutons', 'boutons-pokeball');
